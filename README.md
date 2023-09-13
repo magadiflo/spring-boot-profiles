412f3995895dc3cf8f488db7d343ad582ac9271f
Martín <magadiflo@gmail.com>
2023-09-13 17:29:03 -0500

    Inicio

---
+++ README.md
@@ -0,0 +1,6 @@
+# Perfiles en Spring Boot
+
+Fuente: [javatechonline](https://javatechonline.com/profiles-in-spring-boot/?fbclid=IwAR1Etc8VN0AAfmvPSD4dau-OzzP6HVnu5cO4hhUjBZvwnW2B5YQ4O9o6_hU)
+
+---
+
---
+++ src/test/java/com/magadiflo/profiles/app/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magadiflo.profiles.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
